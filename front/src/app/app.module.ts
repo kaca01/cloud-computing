7692cb7ac9b33096ddc1f2c047509defaec5daad
Hristina Adamovic <hristinacina@gmail.com>
2023-04-30 17:07:46 +0200

    initialized project

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from 'src/infrastructure/material.module';
+
+import { AppRoutingModule } from '../infrastructure/app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front/src/infrastructure/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/app-root', pathMatch: 'full' },
+  { path: '**', component: AppComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
