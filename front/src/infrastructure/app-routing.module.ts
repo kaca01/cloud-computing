902f61b493134e2f12e2276a5342e1fd0e8a2d34
Hristina Adamovic <hristinacina@gmail.com>
2023-05-02 16:39:17 +0200

    changed app logo and title

--- front/src/infrastructure/app-routing.module.ts
+++ front/src/infrastructure/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
+import { WelcomePageComponent } from 'src/app/components/welcome-page/welcome-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/app-root', pathMatch: 'full' },
-  { path: '**', component: AppComponent },
+  { path: 'welcome-page', component: WelcomePageComponent},
+  { path: '', redirectTo: '/welcome-page', pathMatch: 'full' },
+  { path: '**', component: WelcomePageComponent },
 ];
 
 @NgModule({
