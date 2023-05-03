5b53cd768611d3dea6ceef1424457aaae64ae3c3
savic-a <anastasijas557@gmail.com>
2023-05-03 21:33:51 +0200

    Refactoring code

--- back/main.py
+++ back/main.py
@@ -1,4 +1,6 @@
 import boto3
+import os
+import datetime
 
 aws_access_key = 'AKIAXTEDOKGSG3RZHLUF'
 aws_secret_access_key = '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'
@@ -8,38 +10,47 @@ session = boto3.Session(
     aws_secret_access_key=aws_secret_access_key
 )
 
-def hello_s3():
-    # Creating high level object for interacting with S3 service
-    s3_client = session.client('s3')
+# Creating high level object for interacting with S3 service
+s3_client = session.client('s3')
 
-    bucket_name = 'kofetina'
+bucket_name = 'nekakofa'
 
+def create_bucket():
     # Creating S3 bucket
     print("=== Create bucket ===")
-    bucket = s3_client.create_bucket(Bucket=bucket_name)
-    
+    return s3_client.create_bucket(Bucket=bucket_name)
+
+def list_buckets():
     # List all existing S3 buckets
     print("=== List buckets ===")
     response = s3_client.list_buckets()
     for bucket in response['Buckets']:
         print(f"\t{bucket['Name']}")
 
-    # Write object to bucket
-    print("=== Write object to bucket ===")
-    file_name = 'object.json'
-    object_key = 'object1'
-    s3_client.upload_file(file_name, bucket_name, object_key)
+def upload_file():
+    # Write file to bucket
+    print("=== Write file to bucket ===")
+    file_path = 'sunflower.png'
+    file_name = 'nekaslika'
 
-    # List all objects in bucket
-    print("=== List all objects in bucket ===")
-    response = s3_client.list_objects(Bucket=bucket_name)
-    for o in response['Contents']:
-        print(f"\t{o['Key']}")
-    
-    # Delete object
-    print("=== Delete object from bucket ===")
-    # s3_client.delete_objects(Bucket=bucket_name, Delete={'Objects': [{'Key': object_key}]})
+    file_size = os.path.getsize(file_path)
+    file_type = file_path.split('.')[1]  
+    file_created_time = datetime.datetime.fromtimestamp(os.path.getctime(file_path)).strftime('%Y-%m-%d %H:%M:%S')
+    file_modified_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path)).strftime('%Y-%m-%d %H:%M:%S')
 
+    s3_client.put_object(
+        Bucket=bucket_name,
+        Key=file_name,
+        Body=open(file_path, 'rb'),
+        ContentType=file_type,
+        ContentLength=file_size,
+        Metadata={
+            'created-time': str(file_created_time),
+            'last-modified-time': str(file_modified_time)
+        }
+    )
+
+def list_objects():
     # List all objects in bucket
     print("=== List all objects in bucket ===")
     response = s3_client.list_objects(Bucket=bucket_name)
@@ -49,6 +60,22 @@ def hello_s3():
     except (KeyError):
         print(f"\tNo content in bucket")
 
+def delete_object(file_name):
+    print("=== Delete object from bucket ===")
+    s3_client.delete_objects(Bucket=bucket_name, Delete={'Objects': [{'Key': file_name}]})
+
+def delete_bucket():
+    # Delete bucket
+    print("=== Delete bucket ===")
+    s3_resource = session.resource('s3')
+    bucket = s3_resource.Bucket(bucket_name)
+    bucket.objects.all().delete()
+    bucket.delete()
 
 if __name__ == '__main__':
-    hello_s3()
+    # create_bucket()
+    # list_buckets()
+    # upload_file()
+    # list_objects()
+    delete_object('nekaslika')
+    delete_bucket()
