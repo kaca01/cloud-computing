aec6218e9389c6f14ff73f8489b7f664b039fab4
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-06-27 16:54:18 +0200

    Folder name binding

--- front/src/app/components/dialogs/create-folder/create-folder.component.ts
+++ front/src/app/components/dialogs/create-folder/create-folder.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Folder } from 'src/app/domain';
 import { FolderService } from 'src/app/services/folder.service';
 import { DocumentsComponent } from '../../documents/documents.component';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-create-folder',
@@ -11,13 +12,14 @@ import { DocumentsComponent } from '../../documents/documents.component';
 })
 export class CreateFolderComponent implements OnInit {
 
-  private name: string = '';
+  name: string = '';
   private path: string = '';
 
   private documentComponent : DocumentsComponent = {} as DocumentsComponent;
 
   constructor(private dialogRef: MatDialogRef<CreateFolderComponent>,
               private service: FolderService,
+              private snackBar: MatSnackBar,
               @Inject(MAT_DIALOG_DATA) data: DocumentsComponent) {
                 this.documentComponent = data;
                 this.path = data.currentPath;  
@@ -27,8 +29,8 @@ export class CreateFolderComponent implements OnInit {
   }
 
   create(): void {
-    this.name = "proba proba 123";
     if (this.name == '') {
+      this.openSnackBar("Please input name of the foder", "Close");
       return;
     }
     this.service.createFolder({
@@ -46,5 +48,19 @@ export class CreateFolderComponent implements OnInit {
     this.dialogRef.close();
     this.documentComponent.currentPath += "/" + this.name;
     this.documentComponent.updateView();
+  }
+
+  close() : void {
+    this.dialogRef.close();
+  }
+
+  openSnackBar(message: string, action: string) {
+    const config: MatSnackBarConfig = {
+      duration: 2000, 
+      panelClass: ['custom-snackbar'],
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    };
+    this.snackBar.open(message, action, config);
   }
 }
