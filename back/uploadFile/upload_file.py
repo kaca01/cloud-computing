0513d5b8f77db3798b1f1f5ad258b07c83c5c116
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-06-28 11:25:23 +0200

    Added check if item is added to table

--- back/uploadFile/upload_file.py
+++ back/uploadFile/upload_file.py
@@ -26,19 +26,20 @@ def lambda_handler(event, context):
 
     max_retries = 3
     retries = 0
-    is_added_to_bucket = False
+    is_added_to_table = False
     while retries < max_retries:
         try:
             # Put item into table
-            table.put_item(Item={'fileName': body["fileName"],
-                                 'fileType': body["fileType"],
-                                 'fileSize': body["fileSize"],
-                                 'fileCreated': body["fileCreated"],
-                                 'fileModified': body["fileModified"],
-                                 'description': body["description"],
-                                 'tags': body["tags"]})
+            if not is_added_to_table:
+                table.put_item(Item={'fileName': body["fileName"],
+                                     'fileType': body["fileType"],
+                                     'fileSize': body["fileSize"],
+                                     'fileCreated': body["fileCreated"],
+                                     'fileModified': body["fileModified"],
+                                     'description': body["description"],
+                                     'tags': body["tags"]})
             # this means that
-            is_added_to_bucket = True
+            is_added_to_table = True
             # Upload file to s3
             decoded_data = base64.b64decode(body["fileContent"].split(',')[1].strip())
             bucket.put_object(Bucket=bucket_name, Key=body["fileName"], Body=decoded_data)
@@ -50,7 +51,7 @@ def lambda_handler(event, context):
                 print(f"Retrying after {backoff_time} seconds...")
                 time.sleep(backoff_time)
             else:
-                if is_added_to_bucket:
+                if is_added_to_table:
                     bucket.delete_objects(Delete={'Objects': [{'Key': body["fileName"]}]})
                 body = {
                     'message': 'Error occurred during file upload'
